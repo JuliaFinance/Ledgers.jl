9803f4a6d57512010f42ce142cc0d09252640f6e
Eric Forgy <Eric.Forgy@coherentasia.com>
2020-03-24 15:25:13 -0700

    CA -> CavalRe

--- src/transactions.jl
+++ src/transactions.jl
@@ -1,8 +1,8 @@
-struct Transaction
+struct Transaction{M}
     _entries::Dict{String,Entry}
-    _module::Function
 end
+Transaction(m,e) = Transaction{m}(e)
 
-function post!(t::Transaction,a::Position)
-    t._module(t._entries,a)
+function post!(t::Transaction{M},a::Position) where M
+    M(t._entries,a)
 end
